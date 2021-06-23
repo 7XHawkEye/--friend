508de4999c0fd77eff92671acdd48dbe6279f6ad
7XHawkEye <sarveshbhayje7530@gmail.com>
2021-06-23 17:18:03 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const logger = require('morgan')
+// const logger = require('morgan')
 const mongoose = require('mongoose')
 const session = require("express-session")
 
@@ -11,7 +11,7 @@ const User = require("./models/user")
 
 
 app.use(express.static(path.join(__dirname, "public")))
-app.use(logger("dev"))
+// app.use(logger("dev"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
